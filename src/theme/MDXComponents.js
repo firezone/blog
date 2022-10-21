8b37adb926e8e0721bf5d6fee882f46459edc929
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-10-21 11:43:41 -0700

    Update post with smaller image

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
+//XXX: Importing this causes an error during 'npm run build'
 //import AsciinemaPlayer from '@site/src/components/AsciinemaPlayer';
 
 export default {
