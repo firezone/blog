e6bd3259caefc7495a1c5ed2aca26377bf0e12b9
Jamil <jamilbk@users.noreply.github.com>
2022-10-21 17:34:20 -0700

    Update post with smaller image (#2)
    * Update post with smaller image
    
    * Fix SSR of AsciinemaPlayer
    
    Co-authored-by: Andrew Rousset <andrew@firezone.dev>

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
-//import AsciinemaPlayer from '@site/src/components/AsciinemaPlayer';
+import AsciinemaPlayer from '@site/src/components/AsciinemaPlayer';
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  //AsciinemaPlayer: AsciinemaPlayer,
+  AsciinemaPlayer: AsciinemaPlayer,
 };
